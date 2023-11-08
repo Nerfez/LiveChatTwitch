1291db37b63e9b16e2a0dd8bc8927894a33434bd
Clément Olivier <oliclems@gmail.com>
2023-11-08 12:30:28 +0100

    Update livechat_test.sql

--- livechat_test.sql
+++ livechat_test.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: ****************
--- Generation Time: Aug 27, 2023 at 05:04 PM
+-- Generation Time: Nov 08, 2023 at 12:28 PM
 -- Server version: 10.6.14-MariaDB
--- PHP Version: 7.4.19
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,47 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `image`
+-- Table structure for table `data`
 --
 
-CREATE TABLE `image` (
+CREATE TABLE `data` (
   `url` text NOT NULL,
-  `ImageTime` text NOT NULL,
-  `ImageTexte` text NOT NULL,
+  `Time` text NOT NULL,
+  `Texte` text NOT NULL,
   `Width` text NOT NULL,
   `Height` text NOT NULL,
-  `Audio` text NOT NULL
+  `Audio` text NOT NULL,
+  `username` text NOT NULL,
+  `avatar` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `image`
+-- Dumping data for table `data`
 --
-
-INSERT INTO `image` (`url`, `ImageTime`, `ImageTexte`, `Width`, `Height`, `Audio`) VALUES
-('', '20000', ' ', '1920', '1080', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `video`
---
-
-CREATE TABLE `video` (
-  `VideoURL` text NOT NULL,
-  `VideoTexte` text NOT NULL,
-  `VideoTime` text NOT NULL,
-  `Height` text NOT NULL,
-  `Width` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `video`
---
-
-INSERT INTO `video` (`VideoURL`, `VideoTexte`, `VideoTime`, `Height`, `Width`) VALUES
-('', ' ', '30000', '480', '854');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
