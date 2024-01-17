3b27fc5dc4522cba98761d3c6f7420fd8d1e5067
Clément Olivier <oliclems@gmail.com>
2024-01-17 15:10:22 +0100

    Update deleteValue.php

--- LiveChat/deleteValue.php
+++ LiveChat/deleteValue.php
@@ -1,5 +1,10 @@
 <?php
-$bdd = new PDO();
+$bdd = new PDO('mysql:host=localhost;dbname=livechat_test', "root", ""); //A remplacer par vos valeurs
+//remplacer localhost par votre ip / nom de serveur
+//livechat_test devrait etre bon si vous avez repris mon dump sql
+//root par votre username bdd
+//le dernier parametre "" par votre mot de passe bdd
+
 $sql = 'DELETE FROM data LIMIT 1'; //on supprime la video / photo en cours
 
 $statement = $bdd->prepare($sql);
